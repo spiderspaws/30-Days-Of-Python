ad66130d4c3864e2f4d3387badda8bf675542b31
Renesmee <m.renesmee.perez@gmail.com>
2026-09-02 17:06:31 +0000

    git commit -m "Day 2 Python: Variables"

---
+++ .github/Exercises/variables.py
@@ -0,0 +1,27 @@
+# Day 2 python coding
+
+first_name = "Ren"
+last_name = "Perez"
+country = "Phillipines" 
+city = "Manila"
+age = 14
+year = 2011
+is_married = False
+skills = ["Art", "Code", "Research", "Literature"]
+
+person_info = {
+    "first_name": "Ren",
+    "last_name": "Perez",
+    "country": "Phillipines",
+    "city": "Manila"
+}
+
+
+print(first_name, last_name, country, age, is_married)
+print('First name:', first_name)
+print('Last name: ', last_name)
+print('Country: ', country)
+print('Age: ', age)
+print('Married: ', is_married)
+
+#-git Day 2 Python: "Variables"
